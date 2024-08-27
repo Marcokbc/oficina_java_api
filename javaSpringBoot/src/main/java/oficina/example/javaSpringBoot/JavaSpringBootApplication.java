b70cfa05e6f29991c22431b969aac92fd6765a55
Marco Antônio Meira de Lima <88397083+Marcokbc@users.noreply.github.com>
2024-08-27 14:46:28 -0300

    First Commit

---
+++ javaSpringBoot/src/main/java/oficina/example/javaSpringBoot/JavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package oficina.example.javaSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootApplication.class, args);
+	}
+
+}
